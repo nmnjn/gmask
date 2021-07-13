4b2bc6a3831d0dc4ca6ad4738c09fc2ac9ee21bb
Naman Jain <naman17@gmail.com>
2021-07-13 13:44:39 +0530

    Updated popup.js

--- popup.js
+++ popup.js
@@ -12,7 +12,6 @@ function ensureSendMessage(tabId, message, callback) {
             chrome.tabs.executeScript(tabId, { file: "content.js" }, function () {
                 if (chrome.runtime.lastError) {
                     console.error(chrome.runtime.lastError);
-                    chrome.runtime.sendMessage(chrome.runtime.id, { target: 'app', type: 'setMessage', body: 'no-product-found', price: 'null' });
                     throw Error("Unable to inject script into tab " + tabId);
                 }
                 // OK, now it's injected and ready
